2f6cbf2f137086fc94df3618219470606c60b74a
Hrithin <100468394+hrithinnnn@users.noreply.github.com>
2023-04-28 20:56:41 +0530

    LoginSignup Validation

--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -1,75 +1,105 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from "@angular/forms";
-import {ErrorStateMatcher} from '@angular/material/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from "@angular/forms";
+import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from "@angular/router";
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-      const isSubmitted = form && form.submitted;
-      return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-    }
-} 
 
 @Component({
     selector: 'login-signup',
     templateUrl: './login-signup.component.html',
     styleUrls: ['./login-signup.component.css'],
 })
-export class LoginSignupComponent {
+export class LoginSignupComponent implements OnInit {
 
-    formGroup: FormGroup;
+    formGroup!: FormGroup;
+    logIn = true;
+    isSubmitted = false;
+    emailExists = false;
 
-    matcher = new MyErrorStateMatcher();
-    logIn:boolean;
-    
 
-    constructor(private route: Router, private routing: ActivatedRoute) {
-        this.logIn=true;
-        this.formGroup = new FormGroup({
-            email: new FormControl('', Validators.email),
-            password: new FormControl('', Validators.required),
-          });
+    constructor(private route: Router, private fb: FormBuilder,) {
+
+            
     }
 
     ngOnInit(): void {
-      
-        this.load()
-      }
-    
-      load() {
 
-        this.showComponents();
+        this.logIn = this.route.url == '/login' ? true : false;
 
-      }
-
-
-    showComponents(){
-        
-        const location = this.route.url; //get route
-        if (location==="/login"){
-            this.logIn=true;
-            
-        }
-        else if(location==="/signup"){
-            this.logIn=false;
-        }
-        console.log(location,this.logIn);
+        this.formGroup = !this.logIn 
+        ?
+        this.fb.group({
+            name: ['', Validators.required],
+            role: ['', [Validators.required]],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+        })
+        :
+        this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+        })
+        this.load();
     }
-    // console.log(this.logIn);
-    
-toggle(){
-    const location = this.route.url; //get route
-        if (location==="/login"){
-            
-            this.route.navigate(['/signup'])
+
+    get isNameTouched(): boolean {
+
+        return this.formGroup.controls['name']?.touched;
+    }
+
+    get isRoleTouched(): boolean {
+
+        return this.formGroup.controls['role']?.touched;
+    }
+
+    get isEmailTouched(): boolean {
+
+        return this.formGroup.controls['email']?.touched;
+    }
+
+    get isPasswordTouched(): boolean {
+
+        return this.formGroup.controls['password']?.touched;
+    }
+
+    getControl(field: string) {
+
+        return this.formGroup.controls[field]
+    }
+
+    load() {
+
+    }
+
+    togglePage() {
+
+        this.route.navigateByUrl(this.logIn ? '/signup' : '/login');
+
+        // const location = this.route.url; //get route
+        // if (location === "/login") {
+
+        //     this.route.navigate(['/signup'])
+
+        // }
+        // else if (location === "/signup") {
+
+        //     this.route.navigate(['/login'])
+        // }
+
+        // console.log(location, this.logIn);
+    }
+
+    submit() {
+        this.isSubmitted = true;
+
+        console.log(this.isNameTouched, this.isRoleTouched, this.isEmailTouched, this.isPasswordTouched);
+
+        if(this.logIn) {
+
+
+        } else {
+
             
         }
-        else if(location==="/signup"){
-            
-            this.route.navigate(['/login']) 
-        }
-        
-        console.log(location,this.logIn);
-}
+    }
 }
